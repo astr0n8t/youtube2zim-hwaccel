1f901e707c06a16731ebabc9039d792c765a1487
Nathan Higley <contact@nathanhigley.com>
2025-09-16 00:39:44 -0500

    fix: try this

--- scraper/src/youtube2zim/custom_presets.py
+++ scraper/src/youtube2zim/custom_presets.py
@@ -16,7 +16,7 @@ class VideoWebmHigh(Config):
     mimetype = f"{preset_type}/webm"
 
     options: ClassVar[dict[str, str | None]] = {
-        "-vf": "scale_vaapi=format=nv12,hwupload",
+        "-vf": "format=nv12|vaapi,hwupload",
         "-codec:v": "vp9_vaapi",  # video codec
         "-codec:a": "libvorbis",  # audio codec
         "-b:a": "48k",  # target audio bitrate
