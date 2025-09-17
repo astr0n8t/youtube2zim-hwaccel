be27a44199f1f466f3115c0148e0a5f1d5b0a8ae
Nathan Higley <contact@nathanhigley.com>
2025-09-17 12:17:03 -0500

    debug: build

--- scraper/src/youtube2zim/processing.py
+++ scraper/src/youtube2zim/processing.py
@@ -60,6 +60,9 @@ def post_process_video(video_dir, video_id, preset, video_format):
 
     dst_path = src_path.with_name(f"video.{video_format}")
     logger.info(f"Reencode video to {dst_path}")
+    print(preset.to_ffmpeg_args())
+    import sys
+    sys.exit()
     success, process = reencode(
         src_path,
         dst_path,
